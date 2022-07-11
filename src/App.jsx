f62437ce3401d8a0263ad9d1480ae3e47d52164f
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-07-10 22:19:03 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+export default function App() {
+  return (
+    <div className="MainContainer">
+      <div className="container">
+        <div className="profile-container">
+          <img src="https://github.com/luisdasilvahenrique.png" alt="" />
+          <h1>Luis Henrique</h1>
+          <p>Front-end Developer</p>
+        </div>
+
+        <div className="description">
+          <p>
+            Sou técnico de informática, atualmente estudando pela plataforma da
+            Rocketseat. 🚀
+          </p>
+          <span>luisenriklh1177027@gmail.com</span>
+        </div>
+
+        <div className="social-icons">
+          <a href="#">Instagram</a>
+          <a href="#">Linkedin</a>
+          <a href="#">Github</a>
+        </div>
+      </div>
+    </div>
+  );
+}
